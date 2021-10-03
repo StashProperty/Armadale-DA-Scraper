30ce1f47b9e8775758735807925a5271873b9bf1
AndreM101 <56064706+AndreM101@users.noreply.github.com>
2021-10-03 22:11:30 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,7 +1,13 @@
 import datetime
 from bs4 import BeautifulSoup
 import requests
-improt scraperwiki
+from sqlalchemy import create_engine
+import pandas as pd
+
+DATABASE = "data.sqlite"
+DATA_TABLE = "data"
+
+engine = create_engine(f'sqlite:///{DATABASE}', echo=False)
 
 raw = requests.get("https://www.armadale.wa.gov.au/public-notices-out-comment")
 soup = BeautifulSoup(raw.content, 'html.parser')
@@ -16,8 +22,11 @@ for row in row_list:
 	description = cells[0].text.strip().split(" - ")
 	if description[0] == "Application for Development Approval":
 		da = {}
-		da['council_reference'] = None
+		da['council_reference'] = 0
 		da['description'] = " - ".join(description)
 		da['address'] = description[-1]
 		da['date_scraped'] = today
 		da_set.append(da)
+
+data = pd.DataFrame(da_set)
+data.to_sql(DATA_TABLE, con=engine, if_exists='append',index=False)
